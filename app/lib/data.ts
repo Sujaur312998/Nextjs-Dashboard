e5acbf6d4d715f88aa6c2bc595999c6989299ae9
Sujaur312998 <suja312998@gmail.com>
2024-07-10 23:58:41 +0600

    chapter 7 done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,13 +9,16 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+// const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+// const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
